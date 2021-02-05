f541ede619f25534b6a3560846ab84431cd4e6cf
Primosz <valadin96@gmail.com>
2021-02-05 17:52:33 +0100

    Working on fitness function

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ def normalize_dataset(dataset):
     return (dataset - min_arr) / (np.amax(dataset, axis=0) - min_arr)
 
 
-def apply_fuzzy_sets():
+def apply_fuzzy_sets(ind):
     domain = linspace(0.0, 1., 100)
-    Short = IT2FS_Gaussian_UncertStd(domain, [0.15, 0.15, 0.1, 1.])
-    Medium = IT2FS_Gaussian_UncertStd(domain, [0.5, 0.15, 0.1, 1.])
-    Long = IT2FS_Gaussian_UncertStd(domain, [0.85, 0.15, 0.1, 1.])
+    Short = IT2FS_Gaussian_UncertStd(domain, [ind[0], ind[1], ind[2], 1.])
+    Medium = IT2FS_Gaussian_UncertStd(domain, [ind[3], ind[4], ind[5], 1.])
+    Long = IT2FS_Gaussian_UncertStd(domain, [ind[6], ind[7], ind[8], 1.])
     IT2FS_plot(Short, Medium, Long, title="Sets",
                legends=["Short", "Medium", "Long"])
     return [Short, Medium, Long]
@@ -167,6 +167,39 @@ def apply_rules_and_variables(IT2FLS, Short, Medium, Long):
     return IT2FLS
 
 
+# s_center, s_spread, s_deviation
+def evaluate_solution(tsk, individual, dataset, actuals):
+    fs = apply_fuzzy_sets(individual)
+    tsk = apply_rules_and_variables(tsk, fs[0], fs[1], fs[2])
+    sum_error = 0
+    for x in range(0, dataset.shape[0]):
+        result = tsk.evaluate({"x1": dataset[x][0], "x2": dataset[x][1], "x3": dataset[x][2], "x4": dataset[x][3]})[
+            'y1']
+        error = abs(result - actuals[x])
+        print(error)
+        sum_error += error
+    return sum_error
+
+
+def main1():
+    iris = datasets.load_iris()
+    normalized_iris = normalize_dataset(iris.data)
+
+    n_features = normalized_iris.shape[1]
+    print("START")
+    myIT2FLS = TSK(product_t_norm, max_s_norm)
+    fuzzy_sets = apply_fuzzy_sets([0.15, 0.15, 0.1, 0.5, 0.15, 0.1, 0.85, 0.15, 0.1])
+    myIT2FLS = apply_rules_and_variables(myIT2FLS, fuzzy_sets[0], fuzzy_sets[1], fuzzy_sets[2])
+
+    for x in range(0, normalized_iris.shape[0]):
+        result = myIT2FLS.evaluate(
+            {"x1": normalized_iris[x][0], "x2": normalized_iris[x][1], "x3": normalized_iris[x][2],
+             "x4": normalized_iris[x][3]})['y1']
+        print(result)
+        print(normalized_iris[x][0], normalized_iris[x][1], normalized_iris[x][2], normalized_iris[x][3],
+              iris.target[x], round(result))
+
+
 def main():
     iris = datasets.load_iris()
     normalized_iris = normalize_dataset(iris.data)
@@ -174,14 +207,7 @@ def main():
     n_features = normalized_iris.shape[1]
     print("START")
     myIT2FLS = TSK(product_t_norm, max_s_norm)
-    fuzzy_sets = apply_fuzzy_sets()
-    myIT2FLS = apply_rules_and_variables(myIT2FLS, fuzzy_sets[0], fuzzy_sets[1], fuzzy_sets[2])
-
-    # print(normalized_iris[149][0], normalized_iris[0][1], normalized_iris[0][2], normalized_iris[0][3])
-
-    print(myIT2FLS.evaluate(
-        {"x1": normalized_iris[149][0], "x2": normalized_iris[149][1], "x3": normalized_iris[149][2],
-         "x4": normalized_iris[149][3]}))
+    evaluate_solution(myIT2FLS, [0.15, 0.15, 0.1, 0.5, 0.15, 0.1, 0.85, 0.15, 0.1], normalized_iris, iris.target)
 
 
 if __name__ == "__main__":
